44e3ab9bf7aa971481679c759d5cf19564be04ce
Boazlarry <boazlarry@gmail.com>
2019-12-05 19:54:45 +0900

    기본 프로젝트
    마라탕 기본프로젝트

---
+++ MRT/Assets/Script/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public PlayerManager player = new PlayerManager();
+
+    public 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        DontDestroyOnLoad (gameObject);
+        instance = this;
+
+		Screen.SetResolution(1920,1080, true);
+		Debug.Log("게임시작");
+
+        player.money = 0;
+        player.date = 0;
+        player.personPTime = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	public void GameStart(string sceneName)
+	{
+        Debug.Log(sceneName+"으로 레벨 이동");
+		SceneManager.LoadScene (sceneName);
+	}
+
+}
+
+
---
+++ MRT/Assets/Script/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static UIManager instance;
+    public Text money;
+    public Text people;
+
+    void Start()
+    {
+        instance=this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        money.text = GameManager.instance.player.money.ToString();
+        people.text = GameManager.instance.player.personPTime.ToString();
+        
+    }
+}
